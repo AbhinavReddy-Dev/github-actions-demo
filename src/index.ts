59dcefa1c18122154cb06536a114b58f00aec00f
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-02-14 10:41:24 -0800

    initial node express ts setup

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+import express from 'express';
+
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (request: express.Request, response: express.Response) => {
+    response.send("Our TypeScript on Node server is running on localhost.");
+});
+
+app.listen(8080, () => {
+  console.log(`Server is running on ${HOST}:${PORT}`);
+});
