b475156d670f020fe153996e28b66d29430eaa16
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-02-21 10:20:38 -0800

    added eslint workflow for the project

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
-import express from 'express';
-
+import express from "express";
 
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 const app = express();
-app.get('/', (request: express.Request, response: express.Response) => {
-    response.send("Our TypeScript on Node server is running on localhost.");
+app.get("/", (request: express.Request, response: express.Response) => {
+  response.send("Our TypeScript on Node server is running on localhost.");
 });
 
 app.listen(8080, () => {
